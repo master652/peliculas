b3c5d066c268c95a4d77deb139dc5c24476c0eae
master652 <master652@hotmail.com>
2023-02-15 18:58:02 -0600

    Mensajes nuevos

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,11 +3,48 @@ import 'package:flutter/material.dart';
 class DetailsScreens extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: Cambiar luego por una instacia de movie
+
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
     return Scaffold(
-      body: Container(
-          child: Center(
-        child: Text('Details Screen'),
-      )),
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+        SliverList(delegate: SliverChildListDelegate([
+          
+        ]))
+      ],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -5,8 +5,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 270,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -16,6 +15,9 @@ class MovieSlider extends StatelessWidget {
               'Populares',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+          ),
+          SizedBox(
+            height: 5,
           ),
           Expanded(
               child: ListView.builder(
@@ -33,21 +35,33 @@ class _MoviePoster extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 130,
-      height: 190,
-      color: Colors.green,
+      height: 250,
       margin: const EdgeInsets.symmetric(
         horizontal: 10,
       ),
-      child: Column(children: const [
-        FadeInImage(
-          placeholder: AssetImage('assets/noimg.jpg'),
-          image: NetworkImage('https://via.placeholder.com/300x400'),
-          width: 130,
-          height: 190,
-          fit: BoxFit.cover,
+      child: Column(children: [
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, 'details',
+              arguments: 'movie-instance'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/noimg.jpg'),
+              image: NetworkImage('https://via.placeholder.com/300x400'),
+              width: 130,
+              height: 190,
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-        Text(
-          'StarWars',
+        const SizedBox(
+          height: 5,
+        ),
+        const Text(
+          'StarWars ssdqwdqw qwddaalsc',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
         )
       ]),
     );
